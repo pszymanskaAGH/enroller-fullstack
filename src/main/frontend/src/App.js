65bc1ee03033c599fb4f43bf6e6e9fc7e8f02500
Student <student@d17-429-pc-02.ipa.ii.agh.edu.pl>
2025-05-25 15:49:54 +0200

    start notify implementation

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,18 +3,28 @@ import './App.css';
 import {useState} from "react";
 import LoginForm from "./LoginForm";
 import UserPanel from "./UserPanel";
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
     const [loggedIn, setLoggedIn] = useState('');
+    const notify = () => toast('Wow so easy !');
 
     function login(email) {
         if (email) {
+            // notifyUser()
+            //TODO notify user
             setLoggedIn(email);
         }
     }
 
     function logout() {
         setLoggedIn('');
+    }
+
+    function notifyUser() {
+        return (
+            notify
+        );
     }
 
     return (
